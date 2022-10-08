c157568a1866658e15d2a9d65cc6923ae9439396
g-dailey <gulafroz.rezai@gmail.com>
2022-10-08 13:00:38 -0700

    Homepage Updated

--- static/js/homepage.jsx
+++ static/js/homepage.jsx
@@ -1,7 +1,13 @@
 'use strict';
 
 function Homepage() {
-  return <div>This component needs to be implemented.</div>;
+  return (
+    <React.Fragment>
+    <p>Welcome!</p>
+    <a href="/cards">Go to the cards page.</a>
+    <img src="/static/img/balloonicorn.jpg" />
+    </React.Fragment>
+    )
 }
 
 ReactDOM.render(<Homepage />, document.querySelector('#app'));
